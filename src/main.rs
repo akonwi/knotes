c862e6add2b7bd6fd92800a188e1a78836db15d7
akonwi <akonwi@gmail.com>
2019-01-23 00:06:47 -0500

    save user to mongo

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 #![feature(proc_macro_hygiene, decl_macro)]
 
 #[macro_use]
+extern crate mongodb;
+#[macro_use]
 extern crate rocket;
 #[macro_use]
 extern crate rocket_contrib;
@@ -9,6 +11,9 @@ extern crate serde_derive;
 #[macro_use]
 extern crate validator_derive;
 
+use bcrypt::{hash, DEFAULT_COST};
+use mongodb::db::ThreadedDatabase;
+use mongodb::Document;
 use rocket_contrib::json::{Json, JsonValue};
 use serde::Serialize;
 use validator::Validate;
@@ -27,46 +32,82 @@ struct User {
     pub email: String,
 }
 
+impl User {
+    pub fn from(doc: Document) -> Self {
+        User {
+            id: doc.get_object_id("_id").ok().unwrap().to_string(),
+            email: doc.get_str("email").unwrap().to_string(),
+        }
+    }
+}
+
 #[derive(Serialize)]
 struct UserResponse {
     user: User,
 }
 
 #[derive(Serialize)]
-enum Status {
-    #[serde(rename = "ok")]
-    Ok,
-    #[serde(rename = "error")]
-    Error,
+struct ErrorResponse {
+    message: String,
 }
 
 fn ok<T: Serialize>(body: T) -> JsonValue {
     json!({
-        "status": Status::Ok,
+        "status": "ok",
         "body": body
     })
 }
 
 fn not_ok<T: Serialize>(body: T) -> JsonValue {
     json!({
-        "status": Status::Error,
+        "status": "error",
         "body": body
     })
 }
 
+fn create_user(conn: &mongodb::db::Database, email: &str, password: &str) -> Result<User, ()> {
+    let hashed_pw = hash(&password, DEFAULT_COST).unwrap();
+
+    let coll = conn.collection("users");
+    let result = coll
+        .insert_one(doc! {"email": email, "password": hashed_pw}, None)
+        .unwrap();
+    match coll
+        .find_one(Some(doc! {"_id": result.inserted_id.unwrap()}), None)
+        .unwrap()
+    {
+        Some(doc) => Ok(User::from(doc)),
+        None => Err(()),
+    }
+}
+
 #[post("/auth/create", data = "<params>")]
-fn register(params: Json<RegistrationParams>) -> JsonValue {
+fn register(params: Json<RegistrationParams>, conn: KnotesDBConnection) -> JsonValue {
     match params.validate() {
-        Ok(_) => ok(json!({
-            "user": User {
-                email: params.email.to_string(),
-                id: String::from("me"),
-            },
-        })),
+        Ok(_) => {
+            let user = match create_user(&conn, &params.email, &params.password) {
+                Ok(user) => user,
+                Err(_) => {
+                    return not_ok(ErrorResponse {
+                        message: String::from("Failed to create User"),
+                    });
+                }
+            };
+
+            ok(json!({
+                "user": user,
+            }))
+        }
         Err(e) => not_ok(e),
     }
 }
 
+#[database("knotes")]
+struct KnotesDBConnection(mongodb::db::Database);
+
 fn main() {
-    rocket::ignite().mount("/", routes![register]).launch();
+    rocket::ignite()
+        .attach(KnotesDBConnection::fairing())
+        .mount("/", routes![register])
+        .launch();
 }
